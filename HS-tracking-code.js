4eaeb8a8b576ea65c4045d791ded1e6c9c47756e
gatorgleam <abbyjane@gatorgleamagency.com>
2025-06-04 09:37:22 -0700

    Update and rename HS-tracking-code.js to HS-tracking-codedemo_button_click.js

--- HS-tracking-code.js
+++
@@ -1,3 +0,0 @@
-<!-- Start of HubSpot Embed Code -->
-<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/44114120.js"></script>
-<!-- End of HubSpot Embed Code -->
---
+++ HS-tracking-codedemo_button_click.js
@@ -0,0 +1,24 @@
+<!-- Start of HubSpot Embed Code -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/44114120.js"></script>
+<!-- End of HubSpot Embed Code -->
+
+<!-- Custom Event Tracking for HubSpot -->
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    var btn = document.getElementById("demo-button"); // Change this ID to match your button
+    if (btn) {
+      btn.addEventListener("click", function () {
+        if (window._hsq) {
+          _hsq.push([
+            "trackEvent",
+            {
+              id: "demo_button_click",
+              value: "User clicked Schedule Demo"
+            }
+          ]);
+        }
+      });
+    }
+  });
+</script>
+
